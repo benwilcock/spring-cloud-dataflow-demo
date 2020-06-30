4606e52eb081ff59688310a31c0af0cfc1d3f54e
Ben Wilcock <benwilcock@gmail.com>
2020-06-30 15:30:57 +0100

    spring-boot:build-image can now be used to create containers. Script added to start them in docker on the same network as the docker-compose servers

---
+++ start-apps-in-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Expects the start-servers.sh script to have been run and servers are waiting."
+echo "Expects that this script has not been run before"
+docker run -d --name loan-source --network spring-cloud-dataflow-demo_default benwilcock/loan-source:1.0.0
+docker run -d --name loan-processor --network spring-cloud-dataflow-demo_default benwilcock/loan-processor:1.0.0
+echo "If you get arrors because the apps already exist"
+echo "Try 'docker start loan-source loan-processor'"
+echo "One running, check the Rabbit console exchanges 'approved' and 'declined' for message activity: http://localhost:15672/#/exchanges"
